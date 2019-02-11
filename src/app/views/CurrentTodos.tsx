cfb2c63256f4e5fcbc77ad5f23f29ff8a164b942
Luke Murray <lukemurray@Lukes-MBP.lan>
2019-02-11 13:24:10 +1100

    simple save to file

--- src/app/views/CurrentTodos.tsx
+++ src/app/views/CurrentTodos.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import TodoList from '../components/TodoList';
 import { TodoItem } from '../components/Todo';
+import * as fs from 'fs'
 
 interface State {
     todos: TodoItem[]
     hasActiveInput: boolean
 }
+
+const FilePath = './todos.json';
 
 export default class CurrentTodos extends React.Component<{}, State> {
     constructor(props: {}) {
@@ -22,16 +25,59 @@ export default class CurrentTodos extends React.Component<{}, State> {
         this.onCancelAddTodo = this.onCancelAddTodo.bind(this)
     }
 
+    componentDidMount() {
+        if (fs.existsSync(FilePath)) {
+            let file = fs.readFileSync(FilePath, 'utf8')
+            if (file) {
+                this.setState({todos: JSON.parse(file)})
+            }
+        }
+    }
+
+    componentDidUpdate(prevProps: {}, prevState: State) {
+        if (this.hasChanges(prevState.todos, this.state.todos)) {
+            console.log('saving file...')
+            this.updateData()
+        }
+    }
+
+    private hasChanges(prevTodos: TodoItem[], todos: TodoItem[]) {
+        if (prevTodos != todos || prevTodos.length != todos.length) {
+            return true
+        }
+        for (let i = 0; i < todos.length; i++) {
+            const prev = prevTodos[i];
+            const todo = todos[i];
+            for (const key in prev) {
+                if (prev.description == 'Email')
+                    console.log(key, (prev as any)[key], (todo as any)[key], (prev as any)[key] != (todo as any)[key])
+                if ((prev as any)[key] != (todo as any)[key]) {
+                    return true
+                }
+            }
+            if (prev != todo)
+                return true
+        }
+        return false
+    }
+
+    private updateData() {
+        fs.writeFileSync(FilePath, JSON.stringify(this.state.todos))
+    }
+
     private onDoneToggle(todo: TodoItem, done: boolean) {
         if (!this.state.todos) {
             return
         }
 
-        const matchTodo = this.state.todos.find(t => t.description == todo.description)
-        if (matchTodo) {
+        const matchTodoIdx = this.state.todos.findIndex(t => t.description == todo.description)
+        if (matchTodoIdx > -1) {
+            let todos = this.state.todos.concat([])
+            let matchTodo = Object.assign({}, todos[matchTodoIdx])
             matchTodo.done = done
+            todos.splice(matchTodoIdx, 1, matchTodo)
+            this.setState({todos: todos})
         }
-        this.setState({todos: this.state.todos})
     }
 
     private onNewTodo(todo: TodoItem, wasEnter: boolean) {
