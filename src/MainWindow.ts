35efe9c1fdf192eb7aa5d0f24ce64e8240020004
Luke Murray <lukemurray@Lukes-MacBook-Pro.local>
2019-02-12 22:54:02 +1100

    package app

--- src/MainWindow.ts
+++ src/MainWindow.ts
@@ -4,7 +4,7 @@ export default class MainWindow extends BrowserWindow {
     constructor() {
         super({ width: 330, height: 500 })
 
-        this.loadURL(process.env.APP_URL || `file://${__dirname}/../index.html`)
+        this.loadURL(process.env.APP_URL || `file://${__dirname}/index.html`)
         this.setTitle('Daily Todo')
     }
 }
